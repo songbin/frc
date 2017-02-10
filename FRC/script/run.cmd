c3e9976bdc4ed029c7dcbda72405a48cd36e5d4d
songbin <songbin.sky@gmail.com>
2017-02-10 10:57:34 +0800

    change from java project to maven project.

--- FRC/script/run.cmd
+++ FRC/script/run.cmd
@@ -1,1 +1,6 @@
-set PATH=%PATH%;.\properties
+set CURR_DIR=%CD%
+set LIB_DIR=%CURR_DIR%\lib
+set CONF_DIR=%CURR_DIR%\properties
+set PATH=%PATH%;%CONF_DIR%
+rem java -classpath %PATH% com.frc.main.Application
+java -Djava.ext.dirs=%LIB_DIR% com.frc.main.Application
